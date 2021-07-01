9e5545ab1812dd8758415bc8cf46edbf73f10f59
bjtlr0 <bjtlr@naver.com>
2021-07-01 16:27:38 +0900

    /

---
+++ src/test/java/com/boot/study/OAuth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.study;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
